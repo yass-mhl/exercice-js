a0aeb693e7b621b6fde35415f7994e7f846c5e8f
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-04-26 13:12:51 +0200

    fin exo 2

---
+++ easy/exo2/main.js
@@ -0,0 +1,11 @@
+const name = 'Toto';
+const age = 30;
+const homme = true;
+
+console.log('Nom = ' + name);
+console.log('Age = ' + age );
+if(homme){
+  console.log("Sexe : homme")
+}else{
+  console.log("Sexe : femme")
+}
