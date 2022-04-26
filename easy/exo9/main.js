844a5daafe58d14bf600331bfb03ffe0734fd3ee
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-04-26 14:38:53 +0200

    fin exo9

---
+++ easy/exo9/main.js
@@ -0,0 +1,7 @@
+var voiture = {
+  immatriculation : "AB1234CD",
+  marque : 'yotota',
+  annee : 2002
+}
+
+console.log(`La voiture d'immatriculation ${voiture.immatriculation}, de marque ${voiture.marque}, date de ${voiture.annee}`);
