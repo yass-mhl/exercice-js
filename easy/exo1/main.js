25405890fd12c7995bb621c885a177f5b72b9bb2
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-04-26 13:06:54 +0200

    1er exo

---
+++ easy/exo1/main.js
@@ -0,0 +1,14 @@
+var a = 1
+var b = 5
+
+console.log("----------Avant Inversion------------")
+console.log("A : "+a)
+console.log("A : "+b)
+
+var rvs = a
+a = b
+b = rvs
+
+console.log("----------Après Inversion------------")
+console.log("A : "+a)
+console.log("A : "+b)
