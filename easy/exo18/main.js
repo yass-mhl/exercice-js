dc3d31affd7b1c90f56069d5e5b03447b148569b
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-04-27 12:50:50 +0200

    fin exo18

---
+++ easy/exo18/main.js
@@ -0,0 +1,7 @@
+var addition = 0;
+
+for (let i = 0; i <= 10; i++) {
+  addition += i;
+  console.log(`Etape ${i} : ${addition}`);
+}
+console.log(`Le résultat des 10 premiers chiffres est égal à 55`);
